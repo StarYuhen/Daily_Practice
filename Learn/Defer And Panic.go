e12911ad4407974b2a5ade25e182ba3014c07bce
StarYuhen <3446623843@qq.com>
2021-07-18 13:50:11 +0800

    增加select的测试案例

---
+++ Learn/Defer And Panic.go
@@ -0,0 +1,6 @@
+package main
+
+//测试defer执行顺序
+func main() {
+
+}
---
+++ Learn/select.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+//测试当条件同时满足时的情况
+func main() {
+	//创建通道通信
+	ch := make(chan int)
+	//发送通信数据
+	go func() {
+		for {
+			ch <- 10
+		}
+	}()
+
+	//select监听
+	for {
+		select {
+		case i := <-ch:
+			fmt.Println(i, "---case1")
+		case <-ch:
+			fmt.Println("---case2")
+		default:
+			fmt.Println("yyyyy---case3")
+		}
+	}
+
+	//经过实验，我们可以发现当多个条件同时满足时，select会随机执行
+}
