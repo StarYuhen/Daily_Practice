143d5cdc8a93c15c0b75270d58994aa1d557528c
StarYuhen <3446623843@qq.com>
2021-10-04 13:58:15 +0800

    力扣签到

--- LeetCode/7. 整数反转.go
+++ LeetCode/7. 整数反转.go
@@ -6,22 +6,22 @@ import (
 )
 
 func main() {
-	fmt.Println(reverse(-2147483648))
+	fmt.Println(reverse(-2147483412))
 }
 
 func reverse(x int) int {
-	xy, res, lens, lenst, lensts := 0, 0, len(strconv.Itoa(x)), 33554431, -33554432
+	xy, res, lens, lenst, lensts := 0, 0, len(strconv.Itoa(x)), 2147483647, -2147483648
 	for i := 1; i <= lens; i++ {
 		// 123 -> 321
-		if res > lenst-1 || res < lensts {
-			return 0
-		}
 		xy = x % 10
 		x = x / 10
 		if i == lens && xy == 0 {
 			return res
 		}
 		res = res*10 + xy
+		if res > lenst-1 || res < lensts {
+			return 0
+		}
 	}
 	return res
 }
