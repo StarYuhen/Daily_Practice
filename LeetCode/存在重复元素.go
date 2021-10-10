315af04d8b7fc06ce8809f79ec26db4e1fdba03e
StarYuhen <3446623843@qq.com>
2021-10-10 17:10:13 +0800

    力扣算法书

---
+++ LeetCode/存在重复元素.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+func containsDuplicate(nums []int) bool {
+	hashmap := map[int]bool{}
+	for _, value := range nums {
+		if hashmap[value] {
+			return true
+		}
+		hashmap[value] = true
+	}
+	return false
+}
