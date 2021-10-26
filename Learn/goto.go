cc04c654b259687885a07a7aec46f1a38d3a6bcb
StarYuhen <3446623843@qq.com>
2021-10-26 14:05:46 +0800

    增加goto语句使用

--- Learn/goto.go
+++ Learn/goto.go
@@ -1,26 +1,14 @@
 package main
 
-import "fmt"
+import "log"
 
 func main() {
-	/* 定义局部变量 */
-	var a int = 10
+P:
+	log.Println("sdfdsgdfg")
 
-	/* 循环 */
 LOOP:
-	for true {
-		if a == 15 {
-			/* 跳过迭代 */
-			a = a + 1
-			goto LOOP
-		}
-		fmt.Printf("a的值为 : %d\n", a)
-		a++
-	}
-	fmt.Println(a)
+	log.Println("dgdfsgdfh")
+	goto P
 
-SP:
-	if a == 100 {
-		goto SP
-	}
+	goto LOOP
 }
