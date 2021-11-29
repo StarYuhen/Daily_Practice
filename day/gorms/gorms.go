0c6a0205e21a8688f0cc2e4fe3e1366a4bc1bcd3
StarYuhen <3446623843@qq.com>
2021-11-29 18:40:39 +0800

    增加绑定文件内容到变量骚操作

--- day/gorms/gorms.go
+++ day/gorms/gorms.go
@@ -1,4 +1,4 @@
-package gorms
+package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
@@ -6,7 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//用于对本地数据可，db文件进行操作 sqlite
+// 用于对本地数据可，db文件进行操作 sqlite
 
 type GORMS struct {
 	gorm.Model
@@ -16,8 +16,8 @@ type GORMS struct {
 
 // User 使用改结构体创建数据表，创建的表名是蛇形，这个创建的名称叫users
 type User struct {
-	ID        string //id会自动生成索引
-	Account   string `gorm:"primaryKey"` //`gorm:"primarykey"` 会将值变为主键
+	ID        string // id会自动生成索引
+	Account   string `gorm:"primaryKey"` // `gorm:"primarykey"` 会将值变为主键
 	Password  string
 	Authority int
 }
@@ -27,17 +27,17 @@ type USER struct {
 	ID string `gorm:"size:255"`
 }
 
-func GormMain() {
-	//初始化数据库链接
+func main() {
+	// 初始化数据库链接
 	db, err := gorm.Open("mysql", "root:abc123456@tcp(localhost:3306)/forum_main?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		logrus.Error(err)
 	}
-	//autoMigrate 自动迁移数据库 也看作自动生成数据表，他并不会把上一个表的数据迁移过来
-	//db.AutoMigrate(&User{})
+	// autoMigrate 自动迁移数据库 也看作自动生成数据表，他并不会把上一个表的数据迁移过来
+	// db.AutoMigrate(&User{})
 	db.AutoMigrate(&USER{})
 	list1 := User{Account: "wdnmd", Password: "sdfsd", Authority: 100}
-	//创建一个记录
+	// 创建一个记录
 	result := db.Create(&list1)
 	/*
 		list1.ID             // 返回插入数据的主键
@@ -47,7 +47,7 @@ func GormMain() {
 
 	logrus.Info(result.Error)
 	logrus.Info(result.RowsAffected)
-	//设置表后缀
+	// 设置表后缀
 	db.Set("gorm:gorm_juan", "ENGINE=InnoDB").AutoMigrate(&User{})
 
 	logrus.Info(db.RowsAffected)
