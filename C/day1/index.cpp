63208b5471f354966ba4be0574147cc84c66a590
StarYuhen <3446623843@qq.com>
2022-03-21 22:31:21 +0800

    学习C的第0.1天

--- C/day1/index.cpp
+++ C/day1/index.cpp
@@ -2,9 +2,15 @@
 // Created by 玉衡 on 2022/3/21.
 //
 
+#include <cstdio>
 
+const auto MAX_SPLIE="DSFDS";
 
 int main(){
-    print("ces")
+    auto Code="Name String Char Bit \n";
+    printf(Code);
+    printf("Code Size %d",sizeof(Code));
+    printf(MAX_SPLIE);
+    return 0;
 }
 
