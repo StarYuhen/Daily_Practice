a6704449b05e206907d14b0df2feec9a20d2a12c
StarYuhen <3446623843@qq.com>
2023-01-24 16:17:18 +0800

    新增注释类的表示 暂停更新改为学习视频跟着做项目

--- Java/SpringBootLearn/src/test/java/com/example/springbootlearn/mapper/UserMapperTest.java
+++ Java/SpringBootLearn/src/test/java/com/example/springbootlearn/mapper/UserMapperTest.java
@@ -12,6 +12,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Collections;
 import java.util.List;
 
+
+/*
+测试常用注解
+● @Test :表示方法是测试方法。但是与JUnit4的@Test不同，他的职责非常单一不能声明任何属性，拓展的测试将会由Jupiter提供额外测试
+● @ParameterizedTest :表示方法是参数化测试，下方会有详细介绍
+● @RepeatedTest :表示方法可重复执行，下方会有详细介绍
+● @DisplayName :为测试类或者测试方法设置展示名称
+● @BeforeEach :表示在每个单元测试之前执行
+● @AfterEach :表示在每个单元测试之后执行
+● @BeforeAll :表示在所有单元测试之前执行
+● @AfterAll :表示在所有单元测试之后执行
+● @Tag :表示单元测试类别，类似于JUnit4中的@Categories
+● @Disabled :表示测试类或测试方法不执行，类似于JUnit4中的@Ignore
+● @Timeout :表示测试方法运行如果超过了指定时间将会返回错误
+● @ExtendWith :为测试类或测试方法提供扩展类引用
+ */
+// 可通过assertions类进行断言
+/*
+ 往测试里面注入参数
+@ValueSource: 为参数化测试指定入参来源，支持八大基础类以及String类型,Class类型
+@NullSource: 表示为参数化测试提供一个null的入参
+@EnumSource: 表示为参数化测试提供一个枚举入参
+@CsvFileSource：表示读取指定CSV文件内容作为参数化测试入参
+@MethodSource：表示读取指定方法的返回值作为参数化测试入参(注意方法返回需要是一个流)
+ */
 // 使用测试
 @SpringBootTest
 class UserMapperTest {
