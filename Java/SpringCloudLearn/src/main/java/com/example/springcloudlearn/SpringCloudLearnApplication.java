1bce5f58b1032cb017ceb5a9d02e55fd5a326f51
StarYuhen <3446623843@qq.com>
2023-01-27 16:53:28 +0800

    init SpringBoot3

---
+++ Java/SpringCloudLearn/src/main/java/com/example/springcloudlearn/SpringCloudLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLearnApplication.class, args);
+    }
+
+}
